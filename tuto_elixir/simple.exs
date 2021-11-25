6093cf4f4043979d1f1dadc14863be63fb323636
Samsoum41 <hocine.samy.sh@gmail.com>
2021-11-25 22:39:54 +0100

    First try with tuples, atoms and functions in Elixir

--- tuto_elixir/simple.exs
+++ tuto_elixir/simple.exs
@@ -1,1 +1,13 @@
-IO.puts("Hello world from Elixir")
+types = {:acier, :combat, :dragon, :eau, :electrique, :feu, :glace, :insecte, :normal, :plante, :poison, :psy, :roche, :sol, :spectre, :tenebre, :vol}
+
+defmodule PokemonTool do
+  def get_types([head|tail]) do
+    IO.puts(head)
+    get_types(tail)
+  end
+  def get_types([]) do
+
+  end
+end
+
+PokemonTool.get_types(Tuple.to_list(types))
